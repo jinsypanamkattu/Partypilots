262e0dc3ed591a55e1f12d639d699d888d860a63
Jinsy <jinsypanamkattu@gmail.com>
2025-03-26 03:40:07 -0700

    updated to fix stripe

--- Server/routes/paymentRoutes.js
+++ Server/routes/paymentRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
 router.post("/create-payment-intent", paymentController.createPayment);
+
+// IMPORTANT: Do NOT add express.raw here - it's already added in server.js
+//router.post("/webhook", paymentController.confirmPayment);
+
 router.post("/webhook",express.raw({ type: 'application/json' }), paymentController.confirmPayment);
 
 router.get("/adminListPayments", paymentController.getPaymentsForAdmin);
